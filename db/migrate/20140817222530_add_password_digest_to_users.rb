dc0ccd0628853246b8f2c8a38d7367d3f04f43ae
Chazie9 <charlesharring3@gmail.com>
2014-08-18 12:26:41 -0700

    got a user

--- db/migrate/20140817222530_add_password_digest_to_users.rb
+++ db/migrate/20140817222530_add_password_digest_to_users.rb
@@ -1,5 +1,7 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration
   def change
     add_column :users, :password_digest, :string
+    validates_confirmation_of :password,
+                          if: lambda { |m| m.password.present? }
   end
 end
